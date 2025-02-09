170fe5c57f5b6a46927f1b43e8a36aa6ca7142c8
Helixi0n <111045175+Helixi0n@users.noreply.github.com>
2025-02-09 12:56:59 +0700

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def get_planets():
-    connection = sqlite3.connect('static/planets.db')
-    query = connection.execute("SELECT name FROM planets")
-    data = query.fetchall()
-    data = [item[0] for item in data]
-    return data
-
